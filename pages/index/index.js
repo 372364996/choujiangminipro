550aa29c2830640cb69c2ed94fc34bf1204e6e17
杜鹏 <dupeng.tommy@outlook.com>
2019-05-09 18:35:48 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,80 @@
+//index.js
+//获取应用实例
+const app = getApp()
+
+Page({
+	data: {
+		motto: 'Hello World',
+		userInfo: {},
+		hasUserInfo: false,
+		canIUse: wx.canIUse('button.open-type.getUserInfo'),
+		current: 'homepage'
+	},
+	//事件处理函数
+	bindViewTap: function() {
+		wx.navigateTo({
+			url: '../logs/logs'
+		})
+	},
+	handleChange({
+		detail
+	}) {
+		this.setData({
+			current: detail.key
+		});
+	},
+	onLoad: function() {
+		if (app.globalData.userInfo) {
+			this.setData({
+				userInfo: app.globalData.userInfo,
+				hasUserInfo: true
+			})
+		} else if (this.data.canIUse) {
+			// 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+			// 所以此处加入 callback 以防止这种情况
+			app.userInfoReadyCallback = res => {
+				this.setData({
+					userInfo: res.userInfo,
+					hasUserInfo: true
+				})
+			}
+		} else {
+			// 在没有 open-type=getUserInfo 版本的兼容处理
+			wx.getUserInfo({
+				success: res => {
+					app.globalData.userInfo = res.userInfo
+					this.setData({
+						userInfo: res.userInfo,
+						hasUserInfo: true
+					})
+				}
+			})
+		}
+	},
+	onShow: function(e) {
+		this.setData({
+			msgList: [{
+					url: "url",
+					title: "158****8549抽取纸巾一包"
+				},
+				{
+					url: "url",
+					title: "166****5269抽取电视机一台"
+				},
+				{
+					url: "url",
+					title: "130****6384抽取手机一部"
+				}
+			]
+		});
+	},
+
+	getUserInfo: function(e) {
+		console.log(e)
+		app.globalData.userInfo = e.detail.userInfo
+		this.setData({
+			userInfo: e.detail.userInfo,
+			hasUserInfo: true
+		})
+	}
+})
